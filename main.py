beb6830cb0f5213994ed597a91e8bf5ace8f6a8d
Jonathan Felix <j.jonathanlima@hotmail.com>
2020-12-26 12:11:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,29 @@ async def feed():
                 links = table.find_all('td', attrs={'class':'release'})[0]
                 for l in links.find_all('a', href=True):
                     try:
-                        f = open("r.txt")
+                        g = open("r.txt", "r")
                         global l_r
                         l_r = l['href']
-                        if f.read() == l_r:
-                            f.write(l_r)
+                        if g.read() == l_r:
+                            g.close()
                             #print('Parece que não tem nada novo...')
                             await asyncio.sleep(300)
-                        else:
+                        else if g.read() != l_r:
                             channel = discord.utils.get(client.get_all_channels(), guild__name='Kiniga Brasil', name='✶⊷彡recentes')
                             await channel.send('Saiu o **{}** de **{}**!'.format(l.get_text(), t.get_text()))
                             await channel.send('Leia aqui: {}'.format(l_r))
                             print('\n\nSaiu o {} de {}! \n\nLeia aqui: {} '.format(l.get_text(), t.get_text(), l_r))
+                            g.close()
+                            f = open("r.txt", "w")
                             f.write(l_r)
                             l_r = ''
                             f.close()
                             await asyncio.sleep(180)
+                         else:
+                            g.close()
+                            f = open("r.txt", "w")
+                            f.write(l_r)
+                            f.close()
                     except: 
                         print('Não encontrei nenhum link...')
                         await asyncio.sleep(3)
