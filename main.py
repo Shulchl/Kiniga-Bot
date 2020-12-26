d5227531952ae420028c2d16db82fb55ae76757e
Jonathan Felix <j.jonathanlima@hotmail.com>
2020-12-26 12:26:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,43 +18,32 @@ async def feed():
                 links = table.find_all('td', attrs={'class':'release'})[0]
                 for l in links.find_all('a', href=True):
                     try:
-                        g = open("r.txt", "r")
-                        global l_r
+                links = table.find_all('td', attrs={'class':'release'})[0]
+                for l in links.find_all('a', href=True):
+                    try:
+                        f = open("r.txt", "r")
                         l_r = l['href']
-                        if g.read() == l_r:
-                            g.close()
+                        if f.read() == l_r:
+                            f.close()
                             #print('Parece que não tem nada novo...')
                             await asyncio.sleep(300)
-                        elif g.read() != l_r:
+                        else:
                             channel = discord.utils.get(client.get_all_channels(), guild__name='Kiniga Brasil', name='✶⊷彡recentes')
                             await channel.send('Saiu o **{}** de **{}**!'.format(l.get_text(), t.get_text()))
                             await channel.send('Leia aqui: {}'.format(l_r))
                             print('\n\nSaiu o {} de {}! \n\nLeia aqui: {} '.format(l.get_text(), t.get_text(), l_r))
+                            g = open("r.txt", "w")
+                            g.write(l_r)
+                            f.close()
                             g.close()
-                            f = open("r.txt", "w")
-                            f.write(l_r)
                             l_r = ''
-                            f.close()
-                            await asyncio.sleep(180)
-                        else:
-                            g.close()
-                            f = open("r.txt", "w")
-                            f.write(l_r)
-                            f.close()
-                            await asyncio.sleep(180)
+                            await asyncio.sleep(30)
                     except: 
                         print('Não encontrei nenhum link...')
                         await asyncio.sleep(3)
-                    
-                    finally: 
-                        pass
-                        
             except:
                 print('Não encontrei nenhum titulo...')
                 await asyncio.sleep(3)
-                
-            finally: 
-                pass
             
 client.loop.create_task(feed())
 
