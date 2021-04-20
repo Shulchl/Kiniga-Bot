003e1fbbfaa7f318dffea313c69c8856b009ff85
Jonathan Felix <j.jonathanlima@hotmail.com>
2021-04-20 08:56:16 -0300

    Change language

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ f = ''
 @client.event
 async def on_ready():
     try:
-        print(f'Tudo perfeito!')
+        print('Tudo perfeito!')
     except:
-        print(f'Não foi possivel adicionar uma atividade.')
-        return
+        return print('Algo deu errado. Reinicie e tente novamente.')
 
 @client.event
 async def feed():
@@ -42,9 +41,9 @@ async def feed():
                             channel = discord.utils.get(client.get_all_channels(), 
                                                         guild__name='Kiniga Brasil', 
                                                         name='✶⊷彡recentes')
-                            await channel.send('Saiu o **{}** de **{}**!\n\n_**Leia agora**_! {}'.format(l.get_text(), 
-                                                                                                         t.get_text(), 
-                                                                                                         l['href']))
+                            await channel.send('Saiu o **{}** de **{}**!\n{}'.format(l.get_text(), 
+                                                                                     t.get_text(), 
+                                                                                     l['href']))
                             f = l['href']
                             await asyncio.sleep(300)
                     except: 
@@ -55,9 +54,6 @@ async def feed():
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send('Olha, eu chuto que esse comando não exite...')
-        await asyncio.sleep(1)
-        return await ctx.channel.purge(limit=2)
-
 
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
@@ -67,20 +63,21 @@ for filename in os.listdir('./cmds'):
 @commands.is_owner()
 async def load(self, ctx, extension):
     self.load_extension(f'cmds.{extension}')
-    await ctx.send(f"I have loaded the command")
+    await ctx.send("Carreguei os comandos")
 
 @client.command()
 @commands.is_owner()
 async def reload(self, ctx, extension):
     self.unload_extension(f'cmds.{extension}')
     self.load_extension(f'cmds.{extension}')
-    await ctx.send("I have reloaded the command")
+    await ctx.send("Recarreguei os comandos")
 
 @client.command()
 @commands.is_owner()
 async def unload(self, ctx, extension):
     self.unload_extension(f'cmds.{extension}')
-    await ctx.send("I have unloaded the command")
+    await ctx.send("Descarreguei os comandos")
+                
                 
 client.loop.create_task(feed())
 
