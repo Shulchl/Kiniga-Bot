a2a2b3abe65792a935269b93a1b5f66efd2cc7c2
Jonathan Felix <j.jonathanlima@hotmail.com>
2021-04-28 16:52:43 -0300

    Update autoRole.py

--- cogs/autoRole.py
+++ cogs/autoRole.py
@@ -34,7 +34,7 @@ class Role(commands.Cog, name='Cargos'):
     async def edit(self, ctx, role: discord.Role, colour: discord.Colour, *, name = None):
         temp = ctx.message.content
         del temp[0]
-        colour = ' '.join(word for word in temp if not word.startswith('#')) #Cargo
+        colour = ' '.join(word for word in temp if word.startswith('#')) #Cargo
         print(temp[1])
         aRole = []
         a = str(role)
